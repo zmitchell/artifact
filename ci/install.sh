81cc03771ea4e5a6d51c41410c7a2f8d9a702f44
Zach Mitchell <zmitchell@fastmail.com>
2018-08-18 13:43:36 -0400

    Reset CI configuration to stock "trust" template
    Some configuration tweaks were made in order to remove unnecessary build
    targets, and to test the sub-crates. The Appveyor configuration is
    completely untouched, and probably does not work.

--- ci/install.sh
+++ ci/install.sh
@@ -1,12 +1,9 @@
 set -ex
 
 main() {
-    #curl https://sh.rustup.rs -sSf | \
-    #    sh -s -- -y --default-toolchain $TRAVIS_RUST_VERSION
-
     local target=
     if [ $TRAVIS_OS_NAME = linux ]; then
-        target=x86_64-unknown-linux-gnu
+        target=x86_64-unknown-linux-musl
         sort=sort
     else
         target=x86_64-apple-darwin
@@ -16,17 +13,15 @@ main() {
     # This fetches latest stable release
     local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
                        | cut -d/ -f3 \
-                       | grep -E '^v[0-9.]+$' \
+                       | grep -E '^v[0.1.0-9.]+$' \
                        | $sort --version-sort \
                        | tail -n1)
-    echo cross version: $tag
     curl -LSfs https://japaric.github.io/trust/install.sh | \
         sh -s -- \
            --force \
            --git japaric/cross \
            --tag $tag \
-           --target $target \
-           --to $RUST_BIN
+           --target $target
 }
 
 main
--- ci/script.sh
+++ ci/script.sh
@@ -3,28 +3,16 @@
 set -ex
 
 main() {
+    # cross build --target $TARGET
+    # cross build --target $TARGET --release
+
     if [ ! -z $DISABLE_TESTS ]; then
         return
     fi
+    cd $TEST_DIR
+    cross test --target $TARGET
+    cross test --target $TARGET --release
 
-    if [ "$CI_BUILD" = "fast" ]; then
-        echo "Only doing fast build and test"
-        cargo test
-        return 0
-    fi
-
-    export RUST_BACKTRACE=1
-    # TODO: just lint
-    # TODO: just lint -- "--features beta"
-    cargo test
-    # TODO: cargo test --features beta
-    # same command that is used in release
-    # TODO: cross rustc --bin art --target $TARGET --release -- -C lto
-    # TODO: export TARGET_BIN="target/$TARGET/release/art"
-    # test "$(uname)" = "Darwin" && echo "TODO: selenium timeout issue on mac" || \
-    #     py.test web-ui/sel_tests
-    # TODO: just check-fmt
-    # eval "$TARGET_BIN check"
 }
 
 # we don't run the "test phase" when doing deploys
